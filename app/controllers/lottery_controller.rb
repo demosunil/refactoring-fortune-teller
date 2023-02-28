a296b977572892b0c134350b7bb24f3c8913b89e
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-02-28 18:35:22 +0000

    zodiacs done with dynamic root

--- app/controllers/lottery_controller.rb
+++ app/controllers/lottery_controller.rb
@@ -1,4 +1,5 @@
 class LotteryController < ApplicationController
+=begin
   def lucky
     @array_of_numbers = []
     5.times do
@@ -18,4 +19,19 @@ class LotteryController < ApplicationController
     end
     render({ :template => "lottery_templates/unlucky.html.erb"})
   end
+=end  
+def lucky_unlucky
+
+    @num = params.fetch("num")
+
+    @array_of_numbers = []
+    5.times do
+      another_number = rand(1...100)
+      
+      @array_of_numbers.push(another_number)
+    end
+    render({ :template => "lottery_templates/luck.html.erb"})
+  end
+
+
 end
